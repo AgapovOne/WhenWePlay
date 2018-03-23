08f8a48b25e895d8d499eaa6f1f8aa9966901b09
Alex Agapov <agapov.one@gmail.com>
2018-03-23 16:07:34 +0500

    Add framework for UI

---
+++ UIFramework/UIFramework.h
@@ -0,0 +1,19 @@
+//
+//  UIFramework.h
+//  UIFramework
+//
+//  Created by Alex Agapov on 23/03/2018.
+//  Copyright © 2018 Alex Agapov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for UIFramework.
+FOUNDATION_EXPORT double UIFrameworkVersionNumber;
+
+//! Project version string for UIFramework.
+FOUNDATION_EXPORT const unsigned char UIFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <UIFramework/PublicHeader.h>
+
+
